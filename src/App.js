8ac33e3547004803ef7e58dc8eb40f573e8f823b
sandeep <sandeep@poq.com>
2025-09-19 10:14:16 +0100

    trying to see why build is not reflecting

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  CommitId: $Id$ 
+	      CommitId: $Id$ 
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
